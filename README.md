9d64f4658e08d64380aff28cba311e635a7c1a45
Malte Herrmann <malteherrmann.mail@web.de>
2024-09-20 01:03:54 +0200

    initial commit using nix and yt-dlp

---
+++ README.md
@@ -0,0 +1,8 @@
+# Youtube MP3 Downloader
+
+This repository contains my personal setup to download mp3s
+of the highest available quality through the CLI.
+
+As a test for me to get used to the Nix ways,
+it's using a Nix flake to organize the execution in a controlled environment.
+
---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A flake that installs youtube-dl";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.yt-dlp;
+
+        apps.default = {
+          type = "app";
+          program = "${pkgs.yt-dlp}/bin/yt-dlp";
+        };
+
+        devShell = pkgs.mkShell {
+          buildInputs = [ pkgs.yt-dlp ];
+        };
+      });
+}
